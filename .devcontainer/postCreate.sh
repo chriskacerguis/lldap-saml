2a848e03d1a4a8b9234aed859c754efc2bcefe68
Chris Kacerguis <2414647+chriskacerguis@users.noreply.github.com>
2024-04-17 08:38:40 -0500

    chore: first commit

---
+++ .devcontainer/postCreate.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+# Setup host aliases
+sudo $(which hostile) set 127.0.0.1 postgres
+sudo $(which hostile) set 127.0.0.1 redis
+sudo $(which hostile) set 127.0.0.1 ldap
---
+++ README.md
@@ -0,0 +1,3 @@
+# lldap-saml
+
+Simple SAML front-end built to run on top of [lldap/lldap](https://github.com/lldap/lldap)
